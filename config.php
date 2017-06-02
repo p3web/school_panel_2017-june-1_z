736134db3b70fdb7dcca0a91b6b87e4de7e5fc16
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-02 10:59:27 +0430

    create config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peymanvalikhanli
+ * Date: 6/2/17 AD
+ * Time: 10:55
+ */
+require_once "model/database/data.php";
+require_once "model/language/lang.php";
+
+// access files
+require_once "model/access/key_facts_function.php";
+
+
+// control files
+
---
+++ model/access/key_facts_function.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peymanvalikhanli
+ * Date: 5/30/17 AD
+ * Time: 22:25
+ */
+
+class key_facts_func
+{
+
+    public static function languages($id ,$name , $deptname = false){
+        //In Total , people in this class\team can speak\understand  up to  A languages
+
+        if($deptname != false ){
+            $class="class";
+            $understand="speak";
+            //SELECT count( DISTINCT languagename) FROM stafflanguage , staff  where orgid="1" AND teamname="CiTeam" AND deptname="Database"
+            $A = data::selects_col("stafflanguage , staff","count( DISTINCT languagename) as count", "orgid='$id' AND teamname='$name' AND deptname='$deptname'");
+           // $A = data::selects("`stafflanguage`","");
+
+        }else{
+            $class="team";
+            $understand="understand";
+            //SELECT count( DISTINCT languagename) FROM studentlanguage , student  where schoolid="14" AND classname="S1"
+            $A = data::selects_col("studentlanguage , student" , "count( DISTINCT languagename) as count", "schoolid='$id' AND classname='$name'");
+        }
+       // print_r($A);
+        $result = "In Total , people in this $class can $understand  up to  ".$A[0]['count']." languages";
+        return $result ;
+    }
+
+    public static function different_faiths($id ,$name , $deptname = false){
+        //   In total, people in this class\team believe in A different faiths
+        if($deptname != false){
+            $class="class";
+            //SELECT count( DISTINCT religion) FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database"
+            $A = data::selects_col("staff", "count( DISTINCT religion) as count",'orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$depname.'"');
+
+        }else{
+            $class="team";
+            //SELECT count( DISTINCT religion) FROM student  where schoolid="14" AND classname="S1"
+            $A = data::selects_col("student","count( DISTINCT religion) as count" , 'schoolid="'.$id.'" AND classname="'.$name.'"');
+        }
+
+        $result = "In total, people in this $class believe in ".$A[0]['count']." different faiths";
+        return $result ;
+
+    }
+    public static function count_Languages($id ,$name , $deptname = false){
+        //   Languages A, B and C are the most spoken\understood languages in this class\team
+        if($deptname != false){
+            $class="class";
+            $spoken = "spoken";
+            //SELECT languagename ,count(*) FROM `stafflanguage` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by languagename order by COUNT(*) desc LIMIT 3
+            $A = data::selects_col('`stafflanguage`','languagename ,count(*) as count', 'staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by languagename order by COUNT(*) desc LIMIT 3');
+
+        }else{
+            $class="team";
+            $spoken = "understood";
+            //SELECT languagename ,count(*) FROM `studentlanguage` where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by languagename order by COUNT(*) desc LIMIT 3
+            $A = data::selects_col('`studentlanguage`','languagename ,count(*) as count' , 'studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by languagename order by COUNT(*) desc LIMIT 3');
+        }
+        //print_r($A);
+        $result = "Languages ".$A[0]['languagename']."(".$A[0]['count']."), ".$A[1]['languagename']."(".$A[1]['count'].") and ".$A[2]['languagename']."(".$A[2]['count'].") are the most $spoken languages in this $class";
+        return $result ;
+
+    }
+
+    public static function grandparents($id ,$name , $deptname = false){
+            //  In total, people in this class\team are from A different countries, which includes country of birth of all(staff/student, mother, father, grandparents )
+            if($deptname != false){
+                $class="class";
+                $staff = "staff";
+                /*select count(DISTINCT x) from
+                (
+                SELECT staffbirthplace as x ,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by staffbirthplace
+                union all
+                SELECT  stafffatherbirthplace as x ,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by stafffatherbirthplace
+                union ALL
+                SELECT  staffmotherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by staffmotherbirthplace
+                union ALL
+                SELECT  stafffathersfatherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by stafffathersfatherbirthplace
+                union ALL
+                SELECT  stafffathersmotherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by stafffathersmotherbirthplace
+                union ALL
+                SELECT  staffmothersfatherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by staffmothersfatherbirthplace
+                union ALL
+                SELECT  staffmothersmotherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by staffmothersmotherbirthplace)as t1
+                */
+                $A = data::selects_col('(SELECT staffbirthplace as x ,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by staffbirthplace union all SELECT  stafffatherbirthplace as x ,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by stafffatherbirthplace union ALL SELECT  staffmotherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by staffmotherbirthplace union ALL SELECT  stafffathersfatherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by stafffathersfatherbirthplace union ALL SELECT  stafffathersmotherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by stafffathersmotherbirthplace union ALL SELECT  staffmothersfatherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by staffmothersfatherbirthplace union ALL SELECT  staffmothersmotherbirthplace as x,count(*)  from `staffbirthdetails` where staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by staffmothersmotherbirthplace)as t1','count(DISTINCT x) as count',"");
+
+            }else{
+                $class="team";
+                $staff = "student";
+                /*
+                select count(DISTINCT x) from
+                (
+                   SELECT studentbirthplace as x ,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1")  group by studentbirthplace
+                union all
+                SELECT  studentfatherbirthplace as x ,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentfatherbirthplace
+                union ALL
+                SELECT  studentmotherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentmotherbirthplace
+                union ALL
+                SELECT  studentfathersfatherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentfathersfatherbirthplace
+                union ALL
+                SELECT  studentfathersmotherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentfathersmotherbirthplace
+                union ALL
+                SELECT  studentmothersfatherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentmothersfatherbirthplace
+                union ALL
+                SELECT  studentmothersmotherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentmothersmotherbirthplace)as t1
+                */
+
+                $A = data::selects_col('(SELECT studentbirthplace as x ,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'")  group by studentbirthplace union all SELECT  studentfatherbirthplace as x ,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentfatherbirthplace union ALL SELECT  studentmotherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentmotherbirthplace union ALL SELECT  studentfathersfatherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentfathersfatherbirthplace union ALL SELECT  studentfathersmotherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentfathersmotherbirthplace union ALL SELECT  studentmothersfatherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentmothersfatherbirthplace union ALL SELECT  studentmothersmotherbirthplace as x,count(*)  from `studentbirthdetails`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentmothersmotherbirthplace)as t1','count(DISTINCT x) as count','');
+            }
+
+            $result = "In total, people in this $class are from ".$A[0]['count']." different countries, which includes country of birth of all($staff, mother, father, grandparents )";
+            return $result ;
+
+        }
+    public static function Highest_number($id ,$name , $deptname = false){
+            //   Highest number of language count spoken by a particular staff or student.
+            if($deptname != false){
+                $class="class";
+                $staff = "staff";
+                //SELECT staffemailid as s, count(*) FROM stafflanguage where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") group by staffemailid order by count(*) DESC LIMIT 1
+                $A = data::selects_col('stafflanguage','staffemailid as s, count(*) as count','staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") group by staffemailid order by count(*) DESC LIMIT 1');
+
+            }else{
+                $class="team";
+                $staff = "student";
+                //SELECT studentemailid as s, count(*) FROM studentlanguage where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") group by studentemailid order by count(*) DESC LIMIT 1
+                $A = data::selects_col('studentlanguage','studentemailid as s, count(*) as count','studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") group by studentemailid order by count(*) DESC LIMIT 1');
+
+            }
+
+            $result = "Highest number of language count spoken by a particular $staff ".$A[0]['count'].".";
+            return $result ;
+
+    }
+    public static function How_many_number ($id ,$name , $deptname = false){
+            //  How many number  of students/staff are not willing to revile their belief.
+            if($deptname != false){
+                $class="class";
+                $staff = "staff";
+                //SELECT count(*) FROM `staff`  where staffemailid IN(SELECT staffemailid FROM staff where orgid="1" AND teamname="CiTeam" AND deptname="Database") and religion='no religion'
+                $A= data::selects_col('`staff` ','count(*) as count','staffemailid IN(SELECT staffemailid FROM staff where orgid="'.$id.'" AND teamname="'.$name.'" AND deptname="'.$deptname.'") and religion="no religion"');
+
+            }else{
+                $class="team";
+                $staff = "student";
+                //SELECT count(*) FROM `student`  where studentemailid IN(SELECT studentemailid FROM student where schoolid="14" AND classname="S1") and religion='no religion'
+                $A = data::selects_col('`student`','count(*) as count','studentemailid IN(SELECT studentemailid FROM student where schoolid="'.$id.'" AND classname="'.$name.'") and religion="no religion"');
+
+            }
+
+            $result = "How many number  of $staff are not willing to revile their belief? ".$A[0]['count'];
+            return $result ;
+
+    }
+
+
+
+
+}
+
+
+
+
