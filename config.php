29d0b58375cf8d9b3c89cb56525c108f040cb925
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-02 11:24:07 +0430

    create time sheet file

--- config.php
+++ config.php
@@ -5,6 +5,14 @@
  * Date: 6/2/17 AD
  * Time: 10:55
  */
+
+// global function
+ob_start();
+session_cache_expire();
+session_start();
+
+
+// data model files
 require_once "model/database/data.php";
 require_once "model/language/lang.php";
 
---
+++ control/view_teacherpanel.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peymanvalikhanli
+ * Date: 6/2/17 AD
+ * Time: 11:17
+ */
+
