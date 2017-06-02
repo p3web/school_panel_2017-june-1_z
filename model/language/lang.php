69a8f3ac952d6fcdc6f01493df907ec8b0f55f1c
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-02 10:47:59 +0430

    create languge files

---
+++ model/language/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peymanvalikhanli
+ * Date: 3/4/17 AD
+ * Time: 20:58
+ */
+class lang{
+
+   // public static $register_data = 'اطلاعات با موفقیت ثبت شد';
+
+
+}
