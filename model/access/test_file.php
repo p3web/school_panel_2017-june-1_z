2fd0c3a1d54dea74e459ab60d554e042d39a9ec1
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-03 22:19:03 +0430

    none

---
+++ model/access/test_file.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peymanvalikhanli
+ * Date: 6/3/17 AD
+ * Time: 21:00
+ */
+
+require_once 'key_facts_function.php';
+
+PSCO_func::get_lang_count_all_male_fmale($orgId ,$teamName , $deptName);
