0fb57d6cadb666a30112b69f0ce45b8241ef40a4
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-08 21:30:49 +0430

    change pass ui

---
+++ headerteamadmin.php
@@ -0,0 +1,47 @@
+<style>
+    
+.logbutton{
+    border-radius:5px !important;
+    color:#ff8a87;
+    border:2px solid #FFF;
+    background-color:#FFF;
+    
+}
+ .logbutton:link, .logbutton:visited{
+    border-radius:5px !important; 
+    color:#ff8a87;
+    border:2px solid #FFF;
+    background-color:#FFF;
+}
+ 
+ .logbutton:active, .logbutton:hover{
+    border-radius:5px !important; 
+    border:2px solid #FFF;
+    background-color:#ff8a87;
+    color:white !important;
+    
+}
+
+
+</style>
+
+<div class="row"  style="background-color:#ff9999;">
+                <div class="col-sm-1">
+                </div>
+                     	    
+                     	    
+        <div class="col-sm-10 header" style="background-color:#ff9999; height:5.325em; margin-top:1em; ">
+        	    
+        	    <div style="margin-left:2em; margin-right:2em;">
+            
+                    <a href="teamadminpanel.php"><img src="images/logo.png" title="Ancestry Atlas" alt="Ancestry Atlas"></a>
+                    
+                    <a href="teamadminpanel.php?logoutrequest"class="btn btn-default navbar-btn logbutton" style="float:right; border-radius:5px; margin:1.0em 0em; font-weight:bold;  text-decoration:none; color:#ff8a87; ">LOGOUT</a>
+                    <a href="teamadminpanel.php?logoutrequest"class="btn btn-default navbar-btn logbutton" style="float:right; border-radius:5px; margin:1.0em 0em; font-weight:bold;  text-decoration:none; color:#ff8a87; margin-right: 1em;">PROFILE</a>
+                </div>
+        </div>
+        
+        <div class="col-sm-1">  
+                </div>
+        
+</div>
---
+++ model/access/user_profile.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peymanvalikhanli
+ * Date: 6/8/17 AD
+ * Time: 13:26
+ */
+require_once 'PSCO_data.php';
+
+class user_profile
+{
+
+    public static function get_profile($id,$type = "teamadmin"){
+
+        switch($type){
+
+            case "teamadmin":
+                return self::get_teame_profile($id);
+                break;
+
+        }
+
+    }
+
+    private static function get_teame_profile($email){
+        $data = data::selects('`teamadmin`', "`teamemailid` = '$email'" );
+
+        $temp = "
+                <script type='text/javascript'>
+                 function btn_user_profile_edit(){
+                    var btn_edit = $('#btn_profile_edit');
+                    if(btn_edit.attr('name') == 'edit'){
+                        $('.psco_readonly').prop('readonly', false);
+                        btn_edit.html('Save');
+                        btn_edit.attr('name','save');
+                        $('#user_profile_header').html('Edit user profile');
+                    }else{
+                        $('.psco_readonly').prop('readonly', true);
+                        btn_edit.html('Edit');
+                        btn_edit.attr('name','edit');
+                        $('#user_profile_header').html('User profile');
+                    }
+                 }
+</script>
+
+  <h3 id='user_profile_header'> User profile</h3>
+                <hr>
+                <table class='table borderless table-responsive'>
+                <tr><td>Email:</td><td><input  type='email' placeholder='Email' readonly value='".$data[0]['teamemailid']."'><br></td></tr>
+                <tr><td>First name:</td><td><input class='psco_readonly' type='email' placeholder='Email' readonly value='".$data[0]['firstname']."'><br></td></tr>
+                <tr><td>Last name:</td><td><input class='psco_readonly' type='email' placeholder='Email' readonly value='".$data[0]['lastname']."'><br></td></tr>
+                <tr><td>Gender:</td><td><select disabled >
+                        <option value='male' ".($data[0]['gender']== "male"? "selected":"").">male</option>
+                        <option value='female' ".($data[0]['gender']== "female"? "selected":"").">female</option>
+                    </select><br></td></tr>
+                <tr><td>Password:</td><td><button type='button' class='btn btn-warning' >change</button><br></td></tr>
+                <tr><td>Department:</td><td> <select disabled>
+                        <option value='Database' ".($data[0]['deptname']== "Database"? "selected":"").">Database</option>
+                        <option value='HR' ".($data[0]['deptname']== "HR"? "selected":"").">HR</option>
+                        <option value='IT' ".($data[0]['deptname']== "IT"? "selected":"").">IT</option>
+                    </select></td></tr>
+                </table>
+                <br><hr>
+                <button type='button' class='btn btn-danger' data-dismiss='modal'>Close</button>
+                <button id='btn_profile_edit' name='edit' type='button' class='btn btn-warning' onclick='btn_user_profile_edit()'>Edit</button>
+                ";
+        return $temp;
+
+    }
+
+}
