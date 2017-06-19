77971cf87371650ffca5fa48202737343927babc
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-19 13:03:09 +0430

    create one exmple for bilife tab school admin panel

--- access_school_key_fact.php
+++ access_school_key_fact.php
@@ -8,26 +8,28 @@
 
 require_once 'PSCO_data.php';
 
+
 class access_school_key_fact {
 
-    public static function key_facts_to_string(){
+    public static function key_facts_to_string($schoolid , $classname){
 
-        $string_fact = "";
+        $string_fact = "<ul style='color: black; line-height: 200%; font-size: medium;'>";
 
 
-        $data = self::exam1();
+        $data = self::exam1($schoolid , $classname);
        // print_r($data); exit; //this line braekpoint for debuging
 
-        $string_fact .= $data[0]['count'];
+        $string_fact .= "<li> There are ".$data[0]['count']." students in the class, of which D are still awaiting to accept the invitation</li>";
 
 
+        $string_fact.="</ul>";
         return $string_fact;
 
     }
 
-    private static function exam1 (){
+    private static function exam1 ($schoolid , $classname){
         //SELECT count( DISTINCT studentemailid) FROM student where schoolid=&quot;14&quot; AND classname=&quot;S1&quot;
-        $data = data::selects_col("`student`","count( DISTINCT studentemailid) as count", "schoolid=14 AND classname='S1'");
+        $data = data::selects_col("`student`","count( DISTINCT studentemailid) as count", "schoolid= '$schoolid' AND classname='$classname'");
         if (count($data[0]) != 0) {
             return $data;
         } else {
