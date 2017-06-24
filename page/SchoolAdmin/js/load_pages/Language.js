8c3d9f22b4c75c4f0874b2b5b3892969ff6520da
pooria-noruzi <pooria.noruzi@gmail.com>
2017-06-24 15:43:18 +0430

    Merge branch 'master' of https://github.com/p3web/school_panel_2017-june-1_z

--- page/SchoolAdmin/js/load_pages/Language.js
+++ page/SchoolAdmin/js/load_pages/Language.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var Language = {
     Series:[],
     DonutSeries: []
@@ -57,3 +58,64 @@ Language.CreateTableData = function (data) {
     }
     TabelCreateor(TableData, 'langTable');
 };
+=======
+var Language = {
+    Series:[],
+    DonutSeries: []
+};
+
+
+Language.sendAction = function (gender) {
+    if (gender == null){
+        gender = 'all'
+    }
+    ajax.sender_data_json_by_url_callback(Global.url, {
+        act: 'get_languge_by_gender',
+        gender: gender
+    }, Language.getData);
+};
+
+Language.getData = function (Data) {
+    Language.CreateBarData(Data);
+    Language.createDonutData(Data);
+    Language.CreateTableData(Data);
+};
+
+Language.CreateBarData = function (Data) {
+    var series = [{
+        name: 'Language',
+        colorByPoint: true,
+        data: []
+    }];
+    for (var i = 0; i < Data.length; i++) {
+        var a = series[0].data;
+        a.push({name: Data[i].languagename, y: parseInt(Data[i].count)});
+    }
+    Language.Series = series;
+    BarChart('LangChartContainer' , series);
+};
+
+
+Language.createDonutData = function (Data) {
+    var donutSeries = {
+        name: 'Language',
+        data: []
+    };
+    for (var i = 0; i < Data.length; i++) {
+        donutSeries.data.push([Data[i].languagename, parseInt(Data[i].count)]);
+    }
+    donutSeries.data.push({name: 'Proprietary or Undetectable', y: 0.2, dataLabels: {enabled: false}});
+    Language.DonutSeries = donutSeries;
+};
+
+Language.CreateTableData = function (data) {
+    TableData = {
+        thName: ['Language', 'Count'],
+        trData: []
+    };
+    for (var i = 0; i < data.length; i++) {
+        TableData.trData.push([data[i].languagename, data[i].count]);
+    }
+    TabelCreateor(TableData, 'langTable');
+};
+>>>>>>> aef1755e47a99604f69700ecdc90fbd94e63981d
--- team_admin_key_fact.php
+++ team_admin_key_fact.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 /**
@@ -10,4 +11,18 @@
 class team_admin_key_fact
 {
 
+=======
+<?php
+
+/**
+ * Created for Cultural Infusion under the project AncestryAtlas
+ * File: team_admin_key_fact.php
+ * User: Nabi
+ * Date: 22/06/2017
+ * Time: 11:15
+ */
+class team_admin_key_fact
+{
+
+>>>>>>> aef1755e47a99604f69700ecdc90fbd94e63981d
 }
