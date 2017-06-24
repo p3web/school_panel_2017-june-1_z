6e9e7d853837663eabfd536924cda874b29bc350
pooria-noruzi <pooria.noruzi@gmail.com>
2017-06-24 15:43:55 +0430

    Merge branch 'master' of https://github.com/p3web/school_panel_2017-june-1_z

--- team_admin_key_fact.php
+++ team_admin_key_fact.php
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 
 /**
@@ -11,6 +12,20 @@
 class team_admin_key_fact
 {
 
+=======
+<?php
+
+/**
+ * Created for Cultural Infusion under the project AncestryAtlas
+ * File: team_admin_key_fact.php
+ * User: Nabi
+ * Date: 22/06/2017
+ * Time: 11:15
+ */
+class team_admin_key_fact
+{
+
+>>>>>>> aef1755e47a99604f69700ecdc90fbd94e63981d
 =======
 <?php
 
